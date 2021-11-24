d3f4d5b77d93f4cc7c4fed6dc86bba84a9deb21f
north-gale043 <79290624+north-gale043@users.noreply.github.com>
2021-11-24 14:56:21 +0900

    messageモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_055308) do
+ActiveRecord::Schema.define(version: 2021_11_24_055533) do
+
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
